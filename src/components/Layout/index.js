94066555d94daf0ce9144cbfdf881e25e4c088ce
Zia-Ur-Rehman087 <ziyasoomro087@gmail.com>
2024-05-02 14:43:59 +0500

    basic structure

---
+++ src/components/Layout/index.js
@@ -0,0 +1,10 @@
+import Sidebar from '../sidebar'
+import './index.scss'
+export default function Layout(){
+    return (
+    <>
+    <h1>Welcome!! to home</h1>
+    <Sidebar />
+    </>
+    )
+}
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,7 @@
+import './index.scss'
+const Sidebar = ()=>{
+    return(
+<h1> Welcome 2 Sidebar</h1>
+    );
+}
+export default Sidebar
